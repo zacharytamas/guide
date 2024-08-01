69c5a1604991b9b70c0db86acf2cc266de923364
Zachary Jones <zachary@jones.cx>
2024-08-01 11:37:11 -0400

    Wireframing the ActionListItem component

--- app/running/index.tsx
+++ app/running/index.tsx
@@ -4,20 +4,30 @@ import { FlatList, SafeAreaView, StyleSheet, View } from "react-native";
 
 interface IAction {
   name: string;
+  /** An estimate of how long this action will take, in seconds. */
+  expectedDuration?: number;
+  /** Elapsed time, in seconds. */
+  elapsedTime: number;
 }
 
 export default function RunningScreen() {
   const [actions, setActions] = useState<IAction[]>([
-    { name: "action 1" },
-    { name: "action 2" },
-    { name: "action 3" },
+    { name: "action 1", expectedDuration: 10, elapsedTime: 0 },
+    { name: "action 2", expectedDuration: 10, elapsedTime: 0 },
+    { name: "action 3", expectedDuration: 10, elapsedTime: 0 },
   ]);
 
   return (
     <View style={styles.container}>
       <FlatList
         data={actions}
-        renderItem={({ item }) => <ActionListItem name={item.name} />}
+        renderItem={({ item }) => (
+          <ActionListItem
+            name={item.name}
+            expectedDuration={item.expectedDuration}
+            elapsedTime={item.elapsedTime}
+          />
+        )}
         keyExtractor={(item) => item.name}
       />
     </View>
--- components/running/ActionListItem.tsx
+++ components/running/ActionListItem.tsx
@@ -1,24 +1,57 @@
 import { Text, View } from "@/components/Themed";
 import { StyleSheet } from "react-native";
 
+const formatTimeToString = (seconds: number): string => {
+  const minutes = Math.floor(seconds / 60);
+  const secondsLeft = seconds % 60;
+  return `${minutes}:${secondsLeft < 10 ? "0" : ""}${secondsLeft}`;
+};
+
 export interface ActionListItemProps {
   name: string;
+  expectedDuration?: number;
+  elapsedTime: number;
 }
 
-export const ActionListItem = ({ name }: ActionListItemProps) => {
+export const ActionListItem = ({
+  name,
+  expectedDuration,
+  elapsedTime,
+}: ActionListItemProps) => {
   return (
     <View style={styles.container}>
-      <Text>{name}</Text>
+      <View style={styles.iconContainer}></View>
+      <View style={styles.actionDetailsContainer}>
+        <Text style={{ fontWeight: "bold" }}>{name}</Text>
+        <Text>Some details</Text>
+      </View>
+      <View>
+        <Text>{formatTimeToString(elapsedTime)}</Text>
+        <Text>{formatTimeToString(expectedDuration ?? 0)}</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    display: "flex",
+    flexDirection: "row",
+    columnGap: 10,
     // NOTE: I'm sure there is a constant somewhere for the standard padding for the platform.
     padding: 10,
     // NOTE: Same here, I'm sure there is a constant for these, possibly even as an object to be spread.
     borderBottomColor: "#ccc",
     borderBottomWidth: 1,
+  },
+  iconContainer: {
+    width: 50,
+    height: 50,
+    backgroundColor: "red",
+    // borderRadius: "100%",
+  },
+  actionDetailsContainer: {
+    flex: 1,
+    flexDirection: "column",
   },
 });
